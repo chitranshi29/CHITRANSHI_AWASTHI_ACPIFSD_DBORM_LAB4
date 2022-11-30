aaa8b158c158572bd84d3e20e7375c1789c4d554
chitranshi <109790176+chitranshi29@users.noreply.github.com>
2022-11-30 20:55:10 +0530

    Add files via upload

---
+++ e_commerce_dbms.sql
@@ -0,0 +1,5 @@
+/*1)	You are required to create tables for supplier,customer,category,product,supplier_pricing,order,rating 
+to store the data for the E-commerce with the schema definition given below*/
+Drop database if exists ecommerce_dbms; /*drop the database if already exist*/
+create database ecommerce_dbms; /*create the e commerce database to store the tables*/
+use ecommerce_dbms;
